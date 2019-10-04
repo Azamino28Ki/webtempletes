cea49f8d3a723ddc74173ce77b9c146918d06326
DESKTOP-A2KUIFE\miyam <miyamizu328@outlook.jp>
2019-10-04 20:19:25 +0900

    Updates

---
+++ precache-manifest.19fcfa4eb9ab2fdf3df810d537138c69.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738293b48c977f7fafe1c12f3aef2216",
+    "url": "/webtempletes/index.html"
+  },
+  {
+    "revision": "f25aab4cf0cde58fdc82",
+    "url": "/webtempletes/static/css/main.18e9b639.chunk.css"
+  },
+  {
+    "revision": "bcde15eb21ccb96a6459",
+    "url": "/webtempletes/static/js/2.7a19725a.chunk.js"
+  },
+  {
+    "revision": "f25aab4cf0cde58fdc82",
+    "url": "/webtempletes/static/js/main.bb6f2de6.chunk.js"
+  },
+  {
+    "revision": "e68b553a08a8cfbfbc52",
+    "url": "/webtempletes/static/js/runtime-main.17550a06.js"
+  },
+  {
+    "revision": "c8bcd2e5810d680ff976ae22e4962ae5",
+    "url": "/webtempletes/static/media/3DRepresation.c8bcd2e5.PNG"
+  },
+  {
+    "revision": "7a8a0d710eab1ec2676e392271cfce41",
+    "url": "/webtempletes/static/media/Gradient_Details.7a8a0d71.PNG"
+  },
+  {
+    "revision": "68a1fb7e91d265542f5efaffd481604a",
+    "url": "/webtempletes/static/media/ListMenue.68a1fb7e.PNG"
+  },
+  {
+    "revision": "109c74c97802bb645ce49d3251593db4",
+    "url": "/webtempletes/static/media/Navigation.109c74c9.PNG"
+  },
+  {
+    "revision": "a5bd390d67ea72722fb3e0e71d36d10a",
+    "url": "/webtempletes/static/media/TextAnimation.a5bd390d.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8cefce22266cbf95f9fb679919f3f425.js"
+  "/webtempletes/precache-manifest.19fcfa4eb9ab2fdf3df810d537138c69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webtempletes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
